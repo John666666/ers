88cc4502fed23ec9f3345a7b289fc7fba57cfb32
lijun <lijun@luobotec.com>
2017-09-09 18:25:27 +0800

    实时监控模块中不显示被禁用的终端

--- ers_admin/views.py
+++ ers_admin/views.py
@@ -24,6 +24,7 @@ def index(request):
     context = {"user": request.session.get("user")}
     return HttpResponse(render(request, "index.html", context))
 
+
 def login(request):
     client_id = request.GET["client_id"]
     pwd = request.GET["pwd"]
@@ -39,7 +40,7 @@ def login(request):
                 # 初始化admin账户
                 token = NimUtils.create_nim_user(client_id)
                 if token:
-                    client_list = Client.getall()   #获取所有已存在的用户
+                    client_list = Client.getall()  # 获取所有已存在的用户
                     client = Client.save_client("管理员", client_id, token, status=1)
                     if client:
                         # 新用户保存成功后， 和之前的所有用户添加好友关系
@@ -52,9 +53,11 @@ def login(request):
         ret = {"code": 300, "message": "登录失败!"}
     return HttpResponse(json.dumps(ret))
 
+
 def logout(request):
     request.session.clear()
     return HttpResponse(render(request, "logout.html"))
+
 
 def list_client(request):
     u"""
@@ -80,19 +83,22 @@ def list_client(request):
                "client_id": client_id, "client_name": client_name, "status": status}
     return HttpResponse(render(request, "client_list.html", context))
 
+
 def browse_client(request):
     u"""
         不分布查询所有终端
     """
-    client_list = Client.getall().exclude(client_id=sys_constant.admin_count)
+    client_list = Client.getall().exclude(client_id=sys_constant.admin_count).exclude(status=0)
     context = {"client_list": client_list}
     return HttpResponse(render(request, "client_interaction.html", context))
+
 
 def client_locus(request):
     u"""
         查看指定终端轨迹
     """
     return HttpResponse(render(request, "client_locus.html", context={}))
+
 
 def nim_call(request):
     u"""
@@ -101,7 +107,7 @@ def nim_call(request):
     client_id = request.GET["client_id"]
     if client_id is None:
         return HttpResponse(json.dumps({"statusCode": "300", "message": "缺少终端号参数"}))
-    #获取本方账号信息
+    # 获取本方账号信息
     client = Client.getone(client_id=sys_constant.admin_count)
     if client is None:
         return HttpResponse(json.dumps({"statusCode": "300", "message": "请登录后再进行此操作"}))
@@ -110,11 +116,12 @@ def nim_call(request):
     context = {"accid": accid, "token": token, "faccid": client_id}
     return HttpResponse(render(request, "nim_call.html", context))
 
+
 def nim_call_inner(request):
     client_id = request.GET["client_id"]
     if client_id is None:
         return HttpResponse(json.dumps({"statusCode": "300", "message": "缺少终端号参数"}))
-    #获取本方账号信息
+    # 获取本方账号信息
     client = Client.getone(client_id=sys_constant.admin_count)
     if client is None:
         return HttpResponse(json.dumps({"statusCode": "300", "message": "请登录后再进行此操作"}))
@@ -122,6 +129,7 @@ def nim_call_inner(request):
     token = client.token
     context = {"accid": accid, "token": token, "faccid": client_id}
     return HttpResponse(render(request, "nim_call_inner.html", context))
+
 
 def save_client(request):
     u"""
@@ -134,7 +142,7 @@ def save_client(request):
         if client_id:
             token = NimUtils.create_nim_user(client_id)
             if token:
-                client_list = Client.getall()   #获取所有已存在的用户
+                client_list = Client.getall()  # 获取所有已存在的用户
                 client = Client.save_client(client_name, client_id, token, status=1)
                 if client:
                     # 新用户保存成功后， 和之前的所有用户添加好友关系
@@ -142,12 +150,13 @@ def save_client(request):
                         for already_client in client_list:
                             NimUtils.force_add_nim_friends(client_id, already_client.client_id)
 
-                    ret = {"statusCode": "200", "message": "添加终端成功", "callbackType":"closeCurrent"}
+                    ret = {"statusCode": "200", "message": "添加终端成功", "callbackType": "closeCurrent"}
     except Exception, e:
         print e
     if not ret:
         ret = {"statusCode": "300", "message": "添加终端失败"}
     return HttpResponse(json.dumps(ret))
+
 
 def delete_client(request):
     u"""
@@ -167,6 +176,7 @@ def delete_client(request):
         ret = {"statusCode": "300", "message": "添加终端失败"}
     return HttpResponse(json.dumps(ret))
 
+
 def delete_clients(request):
     u"""
         批量删除终端
@@ -184,6 +194,7 @@ def delete_clients(request):
     ret = {"statusCode": "200", "message": "成功删除%s条记录" % (str(count))}
     return HttpResponse(json.dumps(ret))
 
+
 def active_client(request):
     u"""
         激活终端
@@ -199,6 +210,7 @@ def active_client(request):
         print e
     return HttpResponse(json.dumps(ret))
 
+
 def disable_client(request):
     u"""
         锁定终端
@@ -206,11 +218,17 @@ def disable_client(request):
     client_id = request.GET['id']
     try:
         client = Client.getone(id=client_id)
-        NimUtils.disable_nim_user(client.get("client_id"))
-        ret = {"statusCode":"200", "message":"终端已成功注销"}
-    except:
-        ret = {"statusCode":"300", "message":"注销失败"}
+        if NimUtils.disable_nim_user(client.client_id):
+            client.status = 0
+            client.save()
+            ret = {"statusCode": "200", "message": "终端已成功注销"}
+    except Exception,e:
+        print e
+        ret = {"statusCode": "300", "message": "注销失败"}
+    if not ret:
+        ret = {"statusCode": "300", "message": "注销失败"}
     return HttpResponse(json.dumps(ret))
+
 
 def update_client(request):
     u"""
@@ -223,10 +241,11 @@ def update_client(request):
         client.client_name = client_name
         client.update_time = datetime.now()
         client.save()
-        ret = {"statusCode":"200", "message":"更新成功", "callbackType":"closeCurrent"}
+        ret = {"statusCode": "200", "message": "更新成功", "callbackType": "closeCurrent"}
     except:
-        ret = {"statusCode":"300", "message":"更新失败"}
+        ret = {"statusCode": "300", "message": "更新失败"}
     return HttpResponse(json.dumps(ret))
+
 
 def client_to_update(request):
     u"""
@@ -238,6 +257,7 @@ def client_to_update(request):
     context = {"client": client}
     return HttpResponse(render(request, "client_update.html", context))
 
+
 def redirect_html(request):
-    template_name = request.path[request.path.rfind("/")+1:]
+    template_name = request.path[request.path.rfind("/") + 1:]
     return HttpResponse(render(request, template_name))
